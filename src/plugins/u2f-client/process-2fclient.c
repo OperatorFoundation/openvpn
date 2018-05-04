e5e3d6fd643527c8362bdeb47f2bb75a3d793881
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2018-05-04 11:45:50 -0500

    Added curl fetching of challenge

--- src/plugins/u2f-client/process-2fclient.c
+++ src/plugins/u2f-client/process-2fclient.c
@@ -16,8 +16,33 @@
 
 static const char program_name[] = "openvpn-2fclient";
 
+struct MemoryStruct {
+  char *memory;
+  size_t size;
+};
+
+static size_t
+WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+  size_t realsize = size * nmemb;
+  struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+
+  mem->memory = realloc(mem->memory, mem->size + realsize + 1);
+  if(mem->memory == NULL) {
+    /* out of memory! */
+    printf("not enough memory (realloc returned NULL)\n");
+    return 0;
+  }
+
+  memcpy(&(mem->memory[mem->size]), contents, realsize);
+  mem->size += realsize;
+  mem->memory[mem->size] = 0;
+
+  return realsize;
+}
+
 static int
-do_auth_request(const char *packet, size_t len, struct msghdr *msg,
+do_auth_request(u2fh_devs *devs, const char *packet, size_t len, struct msghdr *msg,
                 const char **error)
 {
     int fd;
@@ -33,11 +58,66 @@ do_auth_request(const char *packet, size_t len, struct msghdr *msg,
 
     /* The worst password check ever, redux. */
     int ok = (strcmp(username, password) == 0);
-    return ok ? AUTH_RESPONSE_IMMEDIATE_PERMIT : AUTH_RESPONSE_IMMEDIATE_DENY;
+
+    CURL *curl=curl_easy_init();
+    if(!curl)
+    {
+      curl_easy_cleanup(curl);
+      *error = "Could not initialize libcurl";
+      return AUTH_RESPONSE_ERROR;
+    }
+
+    struct MemoryStruct chunk;
+    chunk.memory = malloc(1);  /* will be grown as needed by the realloc above */
+    chunk.size = 0;    /* no data at this point */
+
+    const char *origin="https://demo.yubico.com/wsapi";
+    char url[1024];
+    sprintf(url, "%s/wsapi/u2f/sign?username=%s&password=%s", origin, username, password);
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
+
+    // Call curl_easy_setopt()?
+    CURLcode curl_result=curl_easy_perform(curl);
+    if(curl_result != CURLE_OK)
+    {
+      free(chunk.memory);
+      curl_easy_cleanup(curl);
+      *error = "Bad result from libcurl fetching response";
+      return AUTH_RESPONSE_ERROR;
+    }
+
+    curl_easy_cleanup(curl);
+
+    // Convert to null-terminated string
+    char *challenge=malloc(chunk.size+1);
+    memcpy(challenge, chunk.memory, chunk.size);
+    memset(challenge, 0, chunk.size);
+    free(chunk.memory);
+
+    char response[2048];
+    size_t response_len = sizeof (response);
+
+    u2fh_rc result = u2fh_authenticate2(devs, challenge, origin,
+             response, &response_len,
+             U2FH_REQUEST_USER_PRESENCE);
+
+    free(challenge);
+
+    if(result == U2FH_OK)
+    {
+      return AUTH_RESPONSE_IMMEDIATE_PERMIT;
+    }
+    else
+    {
+      return AUTH_RESPONSE_IMMEDIATE_DENY;
+    }
 }
 
 static void
-control_loop(int sock)
+control_loop(int sock, u2fh_devs *devs)
 {
     for (;;)
     {
@@ -78,7 +158,7 @@ control_loop(int sock)
                 /* return rather than break, to exit loop. */
                 return;
             case OP_AUTH_REQUEST:
-                result = do_auth_request(packet, (size_t)len, &msg, &error);
+                result = do_auth_request(devs, packet, (size_t)len, &msg, &error);
                 switch (result)
                 {
                     case AUTH_RESPONSE_IMMEDIATE_PERMIT:
@@ -173,27 +253,27 @@ main(int argc, char *argv[])
     }
 
     u2fh_initflags flags=U2FH_DEBUG;
-    u2fh_rc init_result = u2fh_global_init(flags);
-    if(init_result != U2FH_OK)
+    u2fh_rc result = u2fh_global_init(flags);
+    if(result != U2FH_OK)
     {
-      fprintf(stderr, "Error initializing u2fh library: %s", u2fh_strerror(init_result));
+      fprintf(stderr, "Error initializing u2fh library: %s", u2fh_strerror(result));
       u2fh_global_done();
       exit(67);
     }
 
     u2fh_devs *devs;
-    init_result = u2fh_devs_init(&devs);
-    if(init_result != U2FH_OK)
+    result = u2fh_devs_init(&devs);
+    if(result != U2FH_OK)
     {
-      fprintf(stderr, "Error initializing u2fh device list: %s", u2fh_strerror(init_result));
+      fprintf(stderr, "Error initializing u2fh device list: %s", u2fh_strerror(result));
       u2fh_global_done();
       exit(68);
     }
 
-    init_result = u2fh_devs_discover(devs, NULL);
-    if(init_result != U2FH_OK)
+    result = u2fh_devs_discover(devs, NULL);
+    if(result != U2FH_OK)
     {
-      fprintf(stderr, "No U2F devices found: %s", u2fh_strerror(init_result));
+      fprintf(stderr, "No U2F devices found: %s", u2fh_strerror(result));
       u2fh_devs_done(devs);
       u2fh_global_done();
       exit(69);
@@ -201,7 +281,7 @@ main(int argc, char *argv[])
 
     comm_2fclient_send_packet(control_socket, OP_INITIALIZED,
                               "b", BACKEND_PROTOCOL_VERSION);
-    control_loop(control_socket);
+    control_loop(control_socket, devs);
     u2fh_devs_done(devs);
     u2fh_global_done();
     return 0;
